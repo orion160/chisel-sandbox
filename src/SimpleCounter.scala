141818ded5ff214e9c17dfa7ac35af1c8e0505c6
Isaac David <idbl64@outlook.com>
2024-12-13 17:44:32 -0500

    Add second counter w/ RegInit

--- src/SimpleCounter.scala
+++ src/SimpleCounter.scala
@@ -15,3 +15,17 @@ class SimpleCounter(maxVal: Int) extends Module {
   count := Mux(reset.asBool, 0.U, applyEn)
   io.out := count
 }
+
+class SimpleCounter2(maxVal: Int) extends Module {
+  require(maxVal > 0)
+
+  val io = IO(new Bundle {
+    val en = Input(Bool())
+    val out = Output(UInt())
+  })
+
+  val count = RegInit(0.U(log2Ceil(maxVal + 1).W))
+  val nextVal = Mux(count < maxVal.U, count + 1.U, 0.U)
+  count := Mux(io.en, nextVal, count)
+  io.out := count
+}
